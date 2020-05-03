cb36e9aab07ccb3f4c6d0a45b0ad07fa71e8961c
WanerValencia <valenciawaner@gmail.com>
2020-05-03 18:55:04 -0500

    Frontend: new Screen Register

---
+++ frontend/src/screens/Register/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Register= props => {
+	return (
+		<div>
+				<h3>Registrando</h3>
+		</div>
+	)
+}
+
+Register.propTypes = {
+
+}
+
+export default Register;
