78280ee4ab17e950bf7a0d1dbb486498ca411d03
WanerValencia <valenciawaner@gmail.com>
2020-05-03 18:44:21 -0500

    Task: add TaskSerializer, add validate and create methods to TaskAPI

--- BACKEND/Schedule/apps/task/serializers.py
+++ BACKEND/Schedule/apps/task/serializers.py
@@ -5,18 +5,27 @@ class TaskAPI(serializers.ModelSerializer):
     class Meta:
         model=Task
         fields="__all__"
-        """def create(self,data):
-            createTask=Task()
-            #print()
-            createTask.title(data.get('title'))
-            createTask.Description(data.get('Description'))
-            createTask.created_by(2)
-            createTask.save()
+    def create(self,data):
+        createTask=Task()
+        #print(data)
+        createTask.title=data.get('title')
+        createTask.Description=data.get('Description')
+        createTask.created_by=data.get('created_by')
+        #createTask.save()
 
-        def validated_data(self,validated_data):
-            task=Task.objects.filter(title=validated_data.get('title'))
-            print(task)
-            if len(task) != 0:
-                raise serializers.ValidationError("Esta tarea ya ha sido creada")
-            else:
-                return task"""
+        return createTask
+
+    def validate(self,validated_data):
+        #print(validated_data)
+        task=Task.objects.filter(created_by=validated_data.get('created_by'),title=validated_data.get('title'))
+        #print(task)
+        if len(task) != 0:
+            print(dir(serializers))
+            raise serializers.ValidationError({"message":"Esta tarea ya ha sido creada"})
+        return validated_data
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Task
+        fields="__all__"
