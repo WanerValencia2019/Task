9298994415aecfb26b836dbe3ea78f84f627a9d4
WanerValencia <valenciawaner@gmail.com>
2020-05-03 18:47:24 -0500

    Task: add insides urls

--- BACKEND/Schedule/apps/task/urls.py
+++ BACKEND/Schedule/apps/task/urls.py
@@ -1,9 +1,16 @@
 from rest_framework import routers
-from .views import TaskView
+from .views import *
+from django.urls import path
 
 
 routes=routers.DefaultRouter()
 
-routes.register(r"",TaskView,"tasks")
+routes.register(r"update",TaskView,"update")
+routes.register(r"delete",TaskView,"delete")
 
-urlpatterns = routes.urls
+urlpatterns=[
+    path(r"list/",TaskA.as_view(),name="tasks2"),
+    path(r"create/",CreateTaskView.as_view(),name="tasks"),
+]
+
+urlpatterns += routes.urls
